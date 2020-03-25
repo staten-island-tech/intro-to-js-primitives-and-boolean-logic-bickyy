1f25693625cfe551f3598300e01dacf5976061d9
bickyy <vicky.zhou21@sitechhs.com>
2020-03-25 10:26:26 -0400

    cw

---
+++ .vscode/app.js
@@ -0,0 +1,87 @@
+// console.log("connected"); 
+//use this to check each time
+
+// let string = "I am a string";
+// let sentence = "Hello Angel" + "and Vicky";
+// let combine = string + sentence
+
+
+// let student = "Sandra"
+// let student2 = "Gia";
+// let couseName= "AP CSP"
+// let teacherName = "Whalen"
+// let sentence = 
+//     student + 
+//     " and " + 
+//     student2 + 
+//     " are talking " + 
+//     courseName " width " + 
+//     teacherName;
+
+// let betterSentence = ${student} and ${student2} are taking ${courseName} with ${teacherName} next year; 
+
+// the ones on top doesnt work and how to format it
+
+// let year = 2020;
+// let nextYear = year-1; 
+// let past = 2020 / 2;
+// let future = year * 2; 
+
+// BooLEANS
+// let trueStatement = "false";
+
+// //NULL AND UNDEFINED 
+// let richard; 
+
+//BooLean Logic or if/else 
+//let age = 18;
+//let baoSquared = "Bao Bao";
+// console.log(baoSquared === "Bao Bao");
+// console.log(age == "17" );
+//triple = is what we use 
+//lower and upper case are diff 
+
+
+// let age = 20;
+// if (age < 18) {
+//     console.log("You are a child");
+// } else if (age == 18){
+//     console.log("happy birthday");
+// } else{
+//     console.log("Your are over 18");
+// }
+
+//red, green, yellow 
+
+// let light = "red"; 
+// if (light === "red") {
+//     console.log("stop");
+// } else if (light === "yellow") {
+//     console.log("caution!");
+// } else {
+//     console.log("go");
+// }
+
+// let kristyIntolerant = true;
+// let baoBaoIntolerant = false; 
+
+// if (kristyIntolerant === true && baoBaoIntolerant === true){
+//     console.log("only almond milk");
+// }
+// else if (kristyIntolerant === true || baoBaoIntolerant === true){
+//     consolse.log("Get both Milks");
+// }else{
+//     console.log("just regular milk");
+// }
+
+// let temperature = 0; 
+// if (temperature < 86) {
+//     console.log("too cold");
+// } else if (temperature < 72 && temperature > 68) {
+//     console.log("perfect");
+// } else if (temperature < 76 && temperature > 73){
+//     console.log("warm");
+// } else if (temperature > 76){
+//     console.log("too hot");
+// }
+
